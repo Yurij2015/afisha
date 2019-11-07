c5e916c31e6e6bcf292ce1b9f9478d0c504bd921
Yurij2015 <nabor_2008@i.ua>
2019-11-08 00:41:57 +0300

    midified artists page

--- public/footer.php
+++ public/footer.php
@@ -1,4 +1,4 @@
-<div class="text-center  container bg-light" style="padding-top: 15px;">
+<div class="text-center  container bg-light" style="padding: 20px 0 10px 0; margin-top: 30px;">
     <p><?php echo "Все права защищены. Афиша &copy; " . " " . date("Y"); ?>
     </p>
 </div>
