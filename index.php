1c01b9ef924e4a3a10cabcfa5754fbf442dea115
Yurij2015 <nabor_2008@i.ua>
2019-11-09 01:58:54 +0300

    set calendar and search on main page

--- index.php
+++ index.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="bootstrap-4.0.0-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css"
+          integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
     <title>Афиша</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
@@ -23,17 +25,37 @@
 <div class="container">
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
         <a class="navbar-brand" href="#">
-            <img src="bird.jpg" alt="Logo" style="width:40px;">
+            <img src="public/images/turandot-140x80.png" alt="Logo" style="width:140px;">
         </a>
+        <ul class="navbar-nav mr-auto">
+            <!-- Dropdown -->
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                    Афиша
+                </a>
+                <div class="dropdown-menu">
+                    <a class="dropdown-item" href="#">Link 1</a>
+                    <a class="dropdown-item" href="#">Link 2</a>
+                    <a class="dropdown-item" href="#">Link 3</a>
+                </div>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">Новости</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Артисты</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Отзывы</a>
+            </li>
+        </ul>
         <ul class="navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link authcolor small" href="#"><i class="fas fa-user"></i> Авторизация</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link authcolor small" href="#">Регистрация</a>
             </li>
         </ul>
     </nav>
@@ -71,8 +93,29 @@
     </div>
     <div class="container">
         <div class="row afisha">
+            <div class="starthead bg-dark">
+                <h1 class="starthead-center">Афиша</h1>
+            </div>
+            <div class="row contentafisha">
+                <div class="calendar contentafisha-center">Календарь</div>
+                <div class="emptyplace"></div>
+                <div class="search">
+                    <form method="post" id="searchrequest">
+                        <label for="search"></label>
+                        <input placeholder="Поиск по спектаклям, артистам" id="search" name="search">
+                        <span><i class="fa fa-search"></i></span>
+                    </form>
+                </div>
 
+                <!--                    <div class="col-md-2">-->
+                <!--                        <p class="text-dark">jkjkj</p>-->
+                <!--                    </div>-->
+                <!--                    <div class="col-md-7">-->
+                <!--                        <p>kjkjkjkk</p>-->
+                <!--                    </div>-->
+            </div>
         </div>
     </div>
+</div>
 </body>
 </html>
